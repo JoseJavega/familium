7cb9bcdcde5142a0a322100ef4c6be3abc71738b
JoseJavega <josejavega@gmail.com>
2025-05-09 19:14:02 +0200

    feat: creada clase PersonModel

---
+++ js/models/PersonModel.js
@@ -0,0 +1,26 @@
+class PersonModel {
+  constructor(db) {
+    this.db = db; // Conexión a la base de datos (instancia de Database)
+    this.table = 'persons'; // Nombre del objectStore
+  }
+
+  async getAll() {
+    return this.db.getAll(this.table);
+  }
+
+  async getById(id) {
+    return this.db.getById(this.table, id);
+  }
+
+  async add(data) {
+    return this.db.add(this.table, data);
+  }
+
+  async update(data) {
+    return this.db.update(this.table, data);
+  }
+
+  async remove(id) {
+    return this.db.remove(this.table, id);
+  }
+}
